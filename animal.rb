b27691f539b57362be30e8106d790f8a6add597e
Erick Contreras <eri89--@hotmail.com>
2023-01-02 18:04:23 -0600

    Add animal class with accessors

---
+++ animal.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Animal
+  attr_reader :id, :type, :number_of_legs
+  attr_accessor :name
+
+  def initialize(type, number_of_legs, name = 'Unknow')
+    @id = Random.rand(1..1000)
+    @name = name
+    @number_of_legs = number_of_legs
+    @type = type
+  end
+end
