b8d0e019dfe8b4ddde34e3231c9e978acc7083c0
Erick Contreras <eri89--@hotmail.com>
2023-01-04 18:27:22 -0600

    Add owner class and examples to main.rb

--- main.rb
+++ main.rb
@@ -3,15 +3,21 @@
 require_relative 'animal'
 require_relative 'dog'
 require_relative 'spider'
+require_relative 'owner'
 
 animal = Animal.new('lion', 4, 'Rex')
 dog = Dog.new('black', 'Rex')
 spider = Spider.new(85, 'Wilma')
 
-p animal.likes_food?('meat')
-p dog.likes_food?('meat')
-p spider.likes_food?('meat')
+alex = Owner.new("Alex")
+alex.animals
+alex.add_animal(dog)
+alex.animals
+alex.add_animal(spider)
+alex.animals
+alex.add_animal(animal)
+alex.animals.map {|animal| animal.name}
 
-p animal.likes_food?('bug')
-p dog.likes_food?('bug')
-p spider.likes_food?('bug')
+alex.animals.count
+alex.animals.first.name
+alex.animals.first.number_of_legs
